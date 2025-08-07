07add8985a77ae70bd96f16ea8b63a7b5a2eef32
Carine Rasamoely <hei.bryan.17@gmail.com>
2025-08-07 12:10:40 +0300

    modification dans la classe combattant

--- src/UFC/Combattant.java
+++ src/UFC/Combattant.java
@@ -1,63 +1,62 @@
 package UFC;
 
-public class Combattant {
-    private String id;
-    private String nom;
-    private String prenom;
-    private String nomDeCombat;
-    private double poids;
-    private String[] titres;
+import java.util.ArrayList;
+import java.util.List;
 
-    public Combattant(String id, String nom, String prenom, String nomDeCombat, double poids, String[] titres) {
+public class Combattant {
+    private final String id;
+    private final String firstName;
+    private final String lastName;
+    private final String nickname;
+    private final double weight;
+    private final List<String> titles;
+    private int wins;
+    private int losses;
+    private int draws;
+
+    public Combattant(String id, String firstName, String lastName, String nickname, double weight) {
         this.id = id;
-        this.nom = nom;
-        this.prenom = prenom;
-        this.nomDeCombat = nomDeCombat;
-        this.poids = poids;
-        this.titres = titres;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.nickname = nickname;
+        this.weight = weight;
+        this.titles = new ArrayList<>();
+        this.wins = 0;
+        this.losses = 0;
+        this.draws = 0;
+    }
+
+    public void addTitle(String title) {
+        titles.add(title);
+    }
+
+    public void updateRecord(boolean won, boolean draw) {
+        if (draw) {
+            draws++;
+        } else if (won) {
+            wins++;
+        } else {
+            losses++;
+        }
     }
 
     public String getId() {
         return id;
     }
-    public void setId(String id) {
-        this.id = id;
-    }
-    public String getNom() {
-        return nom;
-    }
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-    public String getPrenom() {
-        return prenom;
-    }
-    public void setPrenom(String prenom) {
-        this.prenom = prenom;
-    }
-    public String getNomDeCombat() {
-        return nomDeCombat;
-    }
-    public void setNomDeCombat(String nomDeCombat) {
-        this.nomDeCombat = nomDeCombat;
-    }
-    public double getPoids() {
-        return poids;
-    }
-    public void setPoids(double poids) {
-        this.poids = poids;
-    }
-    public String[] getTitres() {
-        return titres;
-    }
-    public void setTitres(String[] titres) {
-        this.titres = titres;
+
+    public List<String> getTitles() {
+        return new ArrayList<>(titles);
     }
 
-    public void updateRecord(boolean winner, boolean defeat) {
-
+    public int getWins() {
+        return wins;
     }
 
-    public void addTitle(String titre) {
+    public int getLosses() {
+        return losses;
+    }
+
+    public int getDraws() {
+        return draws;
     }
 }
