6df2cfabfc8c5d3ccbfe2cf1614e78d1239876b9
Carine Rasamoely <hei.bryan.17@gmail.com>
2025-08-07 11:01:55 +0300

    La classe Combattant

---
+++ src/UFC/Combattant.java
@@ -0,0 +1,56 @@
+package UFC;
+
+public class Combattant {
+    private String id;
+    private String nom;
+    private String prenom;
+    private String nomDeCombat;
+    private double poids;
+    private String[] titres;
+
+    public Combattant(String id, String nom, String prenom, String nomDeCombat, double poids, String[] titres) {
+        this.id = id;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.nomDeCombat = nomDeCombat;
+        this.poids = poids;
+        this.titres = titres;
+    }
+
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getNom() {
+        return nom;
+    }
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    public String getPrenom() {
+        return prenom;
+    }
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+    public String getNomDeCombat() {
+        return nomDeCombat;
+    }
+    public void setNomDeCombat(String nomDeCombat) {
+        this.nomDeCombat = nomDeCombat;
+    }
+    public double getPoids() {
+        return poids;
+    }
+    public void setPoids(double poids) {
+        this.poids = poids;
+    }
+    public String[] getTitres() {
+        return titres;
+    }
+    public void setTitres(String[] titres) {
+        this.titres = titres;
+    }
+}
