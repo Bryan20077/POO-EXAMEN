486884d08e52502773d23bceef919a760ae4359d
Carine Rasamoely <hei.bryan.17@gmail.com>
2025-08-07 11:26:30 +0300

    Ajout de la fonction ajouterCombattants et ajouterMatch

--- src/UFC/LigueDeCombat.java
+++ src/UFC/LigueDeCombat.java
@@ -28,5 +28,25 @@ public class LigueDeCombat {
     public void setMatchs(String[] matchs) {
         this.matchs = matchs;
     }
+    public void ajouterCombattant(Combattant combattant) {
+        String[] nouveauxCombattants = new String[combattants.length + 1];
+        System.arraycopy(combattants, 0, nouveauxCombattants, 0, combattants.length);
+        nouveauxCombattants[combattants.length] = combattant.getId();
+        this.combattants = nouveauxCombattants;
+    }
+    public void ajouterMatch(Match match) {
+        String[] nouveauxMatchs = new String[matchs.length + 1];
+        System.arraycopy(matchs, 0, nouveauxMatchs, 0, matchs.length);
+        nouveauxMatchs[matchs.length] = match.getId();
+        this.matchs = nouveauxMatchs;
+    }
+    @Override
+    public String toString() {
+        return "LigueDeCombat{" +
+                "nom='" + nom + '\'' +
+                ", combattants=" + String.join(", ", combattants) +
+                ", matchs=" + String.join(", ", matchs) +
+                '}';
+    }
 
 }
